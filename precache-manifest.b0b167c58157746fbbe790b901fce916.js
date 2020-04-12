0febc3b76b55edd6c51dc4edc11847b822b3a35b
Sayed Hashim <hashimsayed0@gmail.com>
2020-04-12 18:51:03 +0800

    Updates

---
+++ precache-manifest.b0b167c58157746fbbe790b901fce916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ee37aa7bb8370d14c89ab35e2c7c4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d040d6ecc470adfa506",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "36dc4c21ac5707915aff",
+    "url": "/static/js/2.1288cb2e.chunk.js"
+  },
+  {
+    "revision": "5d040d6ecc470adfa506",
+    "url": "/static/js/main.a3d8771f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b9cbfddbc06deb921c364dc315bcb5fb",
+    "url": "/static/media/profile.b9cbfddb.jpg"
+  }
+]);
